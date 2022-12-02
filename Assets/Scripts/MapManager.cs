1bc2fcae30cf9fa13e8febb389c9c7911636253c
udirose <udirose@wustl.edu>
2022-12-02 11:57:17 -0600

    uploading existing project

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    private static MapManager _instance;
+    public static MapManager Instance => _instance;
+    
+    //outlets
+    public OverlayTile overlayTilePrefab;
+    public GameObject overlayContainer;
+    
+    //tracks overlay tiles
+    public Dictionary<Vector2Int, OverlayTile> overlayMap;
+
+    //singleton
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        overlayMap = new Dictionary<Vector2Int, OverlayTile>();
+        var tileMap = gameObject.GetComponentInChildren<Tilemap>();
+        var bounds = tileMap.cellBounds;
+        
+        //generate map: loops through all drawn tiles on tilemap
+        for (var z = bounds.max.z; z >= bounds.min.z; z--)
+        {
+            for (var y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                for (var x = bounds.min.x; x < bounds.max.x; x++)
+                {
+                    var tileLocation = new Vector3Int(x, y, z);
+                    var tileKey = new Vector2Int(x, y);
+                    if (!tileMap.HasTile(tileLocation) || overlayMap.ContainsKey(tileKey)) continue;
+                    //create overlay and but it one z above
+                    var overlayTile = Instantiate(overlayTilePrefab, overlayContainer.transform);
+                    var cellWorldPosition = tileMap.GetCellCenterWorld(tileLocation);
+                    overlayTile.transform.position = new Vector3(cellWorldPosition.x,cellWorldPosition.y,cellWorldPosition.z+1);
+                    overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder;
+                    overlayMap.Add(tileKey,overlayTile);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
